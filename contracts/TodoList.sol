23e4d5731853fd2d3127ff303646967c0898cfdc
ngyixuan <ngyixuan2009@gmail.com>
2022-11-14 00:53:37 +0800

    create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,24 @@
-pragma solidity  ^0.5.0;
-contract TodoList{
-    uint public taskCount = 0;
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+    mapping(uint256 => Task) public tasks;
+
+    event TaskCreated(uint256 id, string content, bool completed);
+
+    constructor() public {
+        createTask("Check out");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
 }
